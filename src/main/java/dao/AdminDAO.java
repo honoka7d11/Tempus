4f806ac11e80ae009764b77c52827224e97cfaae
holme <holme@192.168.1.19>
2024-12-01 02:04:36 +0900

    ユーザー情報関連

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -6,24 +6,24 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import bean.Admin;
+import bean.Users;
 
 
 
 public class AdminDAO extends DAO {
-	public List<Admin> search() throws Exception{
-		List<Admin> list=new ArrayList<>();
+	public List<Users> search() throws Exception{
+		List<Users> list=new ArrayList<>();
 		
 		Connection con=getConnection();
 		
 		PreparedStatement st;
-		st=con.prepareStatement("SELECT * FROM users;");
+		st=con.prepareStatement("SELECT * FROM users ORDER BY id;");
 		ResultSet rs=st.executeQuery();
 		
 		
 		while (rs.next()) {
-			Admin admin = new Admin();
-			admin.setId(rs.getInt("user_id"));
+			Users admin = new Users();
+			admin.setId(rs.getInt("id"));
 			admin.setName( rs.getString("name"));
 			list.add(admin);
 		}
@@ -33,5 +33,55 @@ public class AdminDAO extends DAO {
 		
 		return list;
 	}
+	
+	public void update(boolean admin, int id) throws Exception{
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st;
+		st=con.prepareStatement("UPDATE users SET = ? WHERE id = ?;");
+		st.setBoolean(1, admin);
+		st.setInt(2, id);
+		
+		st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+	}
+	
+	public void delete(int id) throws Exception{
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st;
+		st=con.prepareStatement("DELETE FROM users WHERE id = ?;");
+		st.setInt(1, id);
+		
+		st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+	}
+	
+	public void insert(Users users) throws Exception{
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st;
+		st=con.prepareStatement("INSERT INTO users VALUES (?, ?, ?, ?);");
+		st.setInt(1, users.getId());
+		st.setString(2, users.getName());
+		st.setString(3, users.getPassword());
+		st.setBoolean(4, users.getAdmin());
+		
+		st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+	}
+	
 
 }
---
+++ src/main/java/employee/UserAction.java
@@ -0,0 +1,13 @@
+package employee;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class UserAction
+ */
+@WebServlet("/UserAction")
+public class UserAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
