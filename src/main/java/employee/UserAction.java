bc89169a7781cc7df4d18d71d4173a05a9bd1155
holme <holme@192.168.1.19>
2024-12-01 22:44:25 +0900

    user機能追加

--- src/main/java/employee/UserAction.java
+++ src/main/java/employee/UserAction.java
@@ -1,13 +1,81 @@
 package employee;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
+import java.util.List;
 
-/**
- * Servlet implementation class UserAction
- */
-@WebServlet("/UserAction")
-public class UserAction extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import bean.Users;
+import dao.AdminDAO;
+import tool.Action;
+
+
+public class UserAction extends Action {
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+		HttpSession session=request.getSession();
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		if (id == 1) {
+			int userId = Integer.parseInt(request.getParameter("selectName"));
+			int admin = Integer.parseInt(request.getParameter("selectAdmin"));
+			if (userId == 0 || admin == 0) {
+				return "users.jsp";
+			}else if (admin == 1) {
+				AdminDAO dao = new AdminDAO();
+				dao.update(true, userId);
+				
+				return "users.jsp";
+			}else if (admin == 2) {
+				AdminDAO dao = new AdminDAO();
+				dao.update(false, userId);
+				
+				return "users.jsp";
+			}else {
+				return "users.jsp";
+			}
+			
+		}else if (id == 2) {
+			int userId = Integer.parseInt(request.getParameter("selectEmp"));
+			
+			if (userId == 0) {
+				return "users.jsp";
+			}
+			
+			AdminDAO dao = new AdminDAO();
+			dao.delete(userId);
+			List<Users> list=dao.search();
+			
+			session.setAttribute("list", list);
+				
+			return "users.jsp";
+			
+			
+		}else if (id == 3){
+			int userId = Integer.parseInt(request.getParameter("user_id"));
+			String name = request.getParameter("user_name");
+			String password = request.getParameter("password");
+			boolean admin = Boolean.valueOf(request.getParameter("admin"));
+			
+			Users users = new Users();
+			users.setId(userId);
+			users.setName(name);
+			users.setPassword(password);
+			users.setAdmin(admin);
+
+			AdminDAO dao = new AdminDAO();
+			dao.insert(users);
+			List<Users> list=dao.search();
+			session.setAttribute("list", list);
+			
+			return "users.jsp";
+		}else {
+			return "login.jsp";
+		}
+		
+		
+	}
 
 }
--- src/main/webapp/employee/users.jsp
+++ src/main/webapp/employee/users.jsp
@@ -5,16 +5,16 @@
 <h1 class=title>社員情報の管理</h1>
 <h2 class=title>管理者登録の登録・削除</h2>
 <div class="userform">
-	<form action="submit.jsp" method="post">
-	        <label for="selectBox">管理者として登録する社員、もしくは管理者登録を解除する社員を選択してください</label>
-	        <select name="selectBox" id="selectBox">
+	<form action="User.action?id=1" method="post">
+	        <label for="selectName">管理者として登録する社員、もしくは管理者登録を解除する社員を選択してください</label>
+	        <select name="selectName" id="selectName">
 	        	<option value=0 hidden>-選択してください-</option>
 	         <c:forEach var="admin" items="${list}">
 	         	<option value="${admin.id}">${admin.id}　${admin.name}</option>
 	         </c:forEach>
 	        </select>
 	        <label for="selectAdmin">「管理者として登録」もしくは「管理者登録を解除」を選んでください</label>
-	        <select name="selectBox" id="selectBox">
+	        <select name="selectAdmin" id="selectAdmin">
 	        	<option value=0 hidden>-選択してください-</option>
 	         	<option value=1>管理者へ追加</option>
 	         	<option value=2>管理者登録を解除</option>
@@ -27,9 +27,9 @@
 
 <h2 class=title>社員情報の削除</h2>
 <div class="userform">
-	<form action="submit.jsp" method="post">
-	        <label for="selectBox">削除する社員を選択してください</label>
-	        <select name="selectBox" id="selectBox">
+	<form action="User.action?id=2" method="post">
+	        <label for="selectEmp">削除する社員を選択してください</label>
+	        <select name="selectEmp" id="selectEmp">
 	        	<option value=0 hidden>-選択してください-</option>
 	         <c:forEach var="admin" items="${list}">
 	         	<option value="${admin.id}">${admin.id}　${admin.name}</option>
@@ -44,9 +44,9 @@
 
 <h2 class=title>新規社員登録</h2>
 <div class="userform">
-	<form action="Return.action" method="post">
+	<form action="User.action?id=3" method="post">
 		<label for="user_id">社員番号</label> <input id="user_id" type="text" name="user_id"required>
-		<label for="name">氏名</label><input id="name" type="text" name="name" required>
+		<label for="user_name">氏名</label><input id="name" type="text" name="user_name" required>
 		<label for="password">パスワード</label><input id="password" name="password" required>
 		<label for="password">管理者権限の有無を選択してください</label>
 		<select>
